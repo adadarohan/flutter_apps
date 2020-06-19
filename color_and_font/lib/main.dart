f0bba53889d9acd24d29a87270379689917c3876
Rohan Kapur <adada.rohan@example.com>
2020-06-19 19:25:05 +0530

    made widgets stateless

--- color_and_font/lib/main.dart
+++ color_and_font/lib/main.dart
@@ -2,27 +2,37 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title : Text("App Bar Title"),
-      centerTitle: true,
-      backgroundColor: Colors.deepPurple[200] ,
-    ),
-    body: Center(
-      child: Text(
-        'hello world !',
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight : FontWeight.bold ,
-          letterSpacing: 2.0,
-          color: Colors.grey[700] ,
-        ),
-        ),
-    ),
-    floatingActionButton: FloatingActionButton( 
-      onPressed: () {},
-      child: Text('Click'),
-      backgroundColor: Colors.deepPurple,
-    ),
-  ),
+  home: House()
 ));
+
+class House extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title : Text("App Bar Title"),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple[200] ,
+      ),
+      body: Center(
+        child: Text(
+          'hello world !',
+          style: TextStyle(
+            fontSize: 50.0,
+            fontWeight : FontWeight.bold ,
+            letterSpacing: 2.0,
+            color: Colors.grey[700] ,
+            fontFamily: 'GreatVibes',
+          ),
+          ),
+      ),
+      floatingActionButton: FloatingActionButton( 
+        onPressed: () {},
+        child: Text('Click'),
+        backgroundColor: Colors.deepPurple,
+      ),
+    
+      
+    );
+  }
+}
