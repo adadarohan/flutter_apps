f25067710746e8ad330deffe8c7ca4fd73586497
Rohan Kapur <adada.rohan@example.com>
2020-06-21 18:51:48 +0530

    added assets

--- images_and_assets/lib/main.dart
+++ images_and_assets/lib/main.dart
@@ -15,16 +15,9 @@ class House extends StatelessWidget {
         backgroundColor: Colors.deepPurple[200] ,
       ),
       body: Center(
-        child: Text(
-          'hello world !',
-          style: TextStyle(
-            fontSize: 50.0,
-            fontWeight : FontWeight.bold ,
-            letterSpacing: 2.0,
-            color: Colors.grey[700] ,
-            fontFamily: 'GreatVibes',
-          ),
-          ),
+        child: Image(
+          image: AssetImage(assetName)
+        ),
       ),
       floatingActionButton: FloatingActionButton( 
         onPressed: () {},
